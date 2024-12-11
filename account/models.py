c52dbb99ec190e2896b12241e3830ea987cba5e9
Renier Perez Gallardo <pere88vdet@gmail.com>
2024-12-11 10:46:26 +0100

    Adedd custom user model.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    profile_picture = models.CharField(max_length=100, blank=True)
+    profile_picture_path = models.CharField(max_length=254, blank=True)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    headline = models.CharField(max_length=200, blank=True)
