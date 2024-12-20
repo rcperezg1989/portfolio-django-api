994812446011f9cdfdabe399af198634ba54b81b
Renier <rcperezg89@gmail.com>
2024-12-20 16:55:00 +0100

    Added register endpoint.

--- account/serializers.py
+++ account/serializers.py
@@ -1,18 +1,8 @@
 from rest_framework import serializers
+from adrf.serializers import Serializer
 from account.models import User
-from django.contrib.auth.hashers import make_password
+from rest_framework.serializers import ValidationError
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
-
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'password', 'first_name', 'last_name']
-
-    def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
-        user = User.objects.create_user(
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
+class RegisterSerializer(Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
--- account/views.py
+++ account/views.py
@@ -3,28 +3,31 @@ from adrf.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from django.contrib.auth.hashers import make_password
 from account.serializers import RegisterSerializer
 from django.db import IntegrityError
+from account.models import User
 
 # Create your views here.
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
     async def post(self, request, *args, **kwargs):
-        print("here")
-        # serializer = RegisterSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     try:
-        #         user = await serializer.save()
-        #         return Response(
-        #             {"message": "User registered successfully", "user_id": user.id},
-        #             status=status.HTTP_201_CREATED,
-        #         )
-        #     except IntegrityError:
-        #         return Response(
-        #             {"error": "A user with this email already exists."},
-        #             status=status.HTTP_400_BAD_REQUEST,
-        #         )
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        json_resp = {"message": "User registered successfully"}
-        return Response(json_resp,status=status.HTTP_201_CREATED,)
+        request.data["password"] = make_password(request.data["password"])
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                user = User()
+                user.email = serializer.validated_data["email"]
+                user.password = make_password(serializer.validated_data["password"])
+                await user.asave()
+                return Response(
+                    {"message": "User registered successfully.", "user_id": user.id},
+                    status=status.HTTP_201_CREATED,
+                )
+            except IntegrityError:
+                return Response(
+                    {"error": "A user with this email already exists."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
